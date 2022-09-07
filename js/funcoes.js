8a25c5babceb0bef34dfde0f169fdf56168a70e7
fatec <fatec@fatec.com>
2022-09-07 14:01:53 -0300

    v02.2

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,6 +1,6 @@
 function selectUniverso(){
     universo = document.querySelector("input[name=universo]:checked").value;
-    document.getElementById("universoHeroi").innerHTML = "Digite um herói do universo " + universo;
+    document.getElementById("universo_heroi").innerHTML = "Digite um herói do universo " + universo;
     document.getElementById("box_universo").style.display = "none";
     document.getElementById("box_heroi").style.display = "block";
 }
@@ -8,80 +8,74 @@ function selectUniverso(){
 function selectHeroi(){
     heroi = document.getElementById("heroi").value;
     if(universo=="DC"){
-        if(heroi.toLowerCase()=="batman"){
-            img_Heroi = "<img src='imgs/dc/batman.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="rorschach"){
-            img_Heroi = "<img src='imgs/dc/rorschach.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="mulher maravilha"){
-            img_Heroi = "<img src='imgs/dc/mulher-maravilha.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="supergirl"){
-            img_Heroi = "<img src='imgs/dc/supergirl.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="superman"){
-            img_Heroi = "<img src='imgs/dc/superman.jpg'>";
-            mostrarHeroi();
-        }else{
-            msg_invalido = "Herói inválido.";
-            msgInvalido();
+        switch(heroi.toLowerCase()){
+            case "batman":
+                img_Heroi = "<img src='imgs/dc/batman.jpg'>";
+                break;
+            case "rorschach":
+                img_Heroi = "<img src='imgs/dc/rorschach.jpg'>";
+                break;
+            case "mulher maravilha": case "mulher-maravilha":
+                img_Heroi = "<img src='imgs/dc/mulher-maravilha.jpg'>";
+                break;
+            case "supergirl":
+                img_Heroi = "<img src='imgs/dc/supergirl.jpg'>";
+                break;
+            case "superman":
+                img_Heroi = "<img src='imgs/dc/superman.jpg'>";
+                break;
+            default:
+                img_Heroi = "Herói inválido.";
+                break;
         }
     }else if(universo=="Marvel"){
-        if(heroi.toLowerCase()=="wolverine"){
-            img_Heroi ="<img src='imgs/marvel/wolverine.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="vampira"){
-            img_Heroi = "<img src='imgs/marvel/vampira.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="motoqueiro fantasma"){
-            img_Heroi = "<img src='imgs/marvel/motoqueiro-fantasma.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="homem de ferro"){
-            img_Heroi = "<img src='imgs/marvel/homem-de-ferro.jpg'>";
-            mostrarHeroi();
-        }else if(heroi.toLowerCase()=="homem aranha"){
-            img_Heroi = "<img src='imgs/marvel/homem-aranha.jpg'>";
-            mostrarHeroi();
-        }else{
-            msg_invalido = "Herói inválido.";
-            msgInvalido();
+        switch(heroi.toLowerCase()){
+            case "wolverine":
+                img_Heroi ="<img src='imgs/marvel/wolverine.jpg'>";
+                break;
+            case "vampira":
+                img_Heroi = "<img src='imgs/marvel/vampira.jpg'>";
+                break;
+            case "motoqueiro fantasma": case "motoqueiro-fantasma":
+                img_Heroi = "<img src='imgs/marvel/motoqueiro-fantasma.jpg'>";
+                break;
+            case "homem de ferro":
+                img_Heroi = "<img src='imgs/marvel/homem-de-ferro.jpg'>";
+                break;
+            case "homem aranha": case "homem-aranha":
+                img_Heroi = "<img src='imgs/marvel/homem-aranha.jpg'>";
+                break;
+            default:
+                img_Heroi = "Herói inválido.";
+                break;
         }
     }else{
-        msg_invalido = "Universo inválido.";
-            msgInvalido();
+        img_Heroi = "Universo inválido.";
     }
-
-    
+    mostrarHeroi();
 }
 
 function mostrarHeroi(){
-    document.getElementById("box_heroi").style.display = "none";
-    document.getElementById("img_heroi").style.display = "flex";
-    document.getElementById("fig_heroi").style.display = "block";
-    document.getElementById("fig_heroi").innerHTML = img_Heroi;
-    document.getElementById("img_heroi").innerHTML += "<button onclick='clonarImagem()'>Mostrar 5x</button>";
-    document.getElementById("img_heroi").innerHTML += "<button onclick='funcVoltar()'>Voltar</button>";
+    if(img_Heroi=="Herói inválido." || img_Heroi=="Universo inválido."){
+        msgInvalido();
+    }else{
+        document.getElementById("box_heroi").style.display = "none";
+        document.getElementById("img_heroi").style.display = "flex";
+        document.getElementById("fig_heroi").style.display = "block";
+        document.getElementById("fig_heroi").innerHTML = img_Heroi;
+        document.getElementById("img_heroi").innerHTML += "<button onclick='funcVoltar()' class='btn'>Voltar</button>";
+    }
 }
 
 function msgInvalido(){
     document.getElementById("box_heroi").style.display = "none";
-    document.getElementById("img_heroi").style.display = "flex";
-    document.getElementById("msg_invalido").style.display = "block";
-    document.getElementById("msg_invalido").innerHTML = "<h1>:(</h1><h3>" + msg_invalido + "</h3>";
-    document.getElementById("img_heroi").innerHTML += "<button onclick='funcVoltar()'>Voltar</button>";
+        document.getElementById("img_heroi").style.display = "flex";
+        document.getElementById("fig_heroi").style.display = "none";
+        document.getElementById("msg_invalido").style.display = "block";
+        document.getElementById("msg_invalido").innerHTML = "<h1>:(</h1><h3>" + img_Heroi + "</h3>";
+        document.getElementById("img_heroi").innerHTML += "<button onclick='funcVoltar()' class='btn'>Voltar</button>";
 }
 
 function funcVoltar(){
     window.location.reload();
-}
-
-function clonarImagem(){
-    document.getElementById("box_heroi").style.display = "none";
-    document.getElementById("img_heroi").style.display = "none";
-    document.getElementById("fig_heroi").style.display = "none";
-    document.getElementById("mostrar5").style.display = "flex";
-    for(var cont=1; cont<=5; cont++){
-        document.getElementById("mostrar5").innerHTML += img_Heroi;
-    }
 }
