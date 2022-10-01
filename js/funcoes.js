6b4ddee166d7c19f51f204ebb02ffe1d74a0a49c
fatec <fatec@fatec.com>
2022-10-01 13:11:01 -0300

    v02.5

--- js/funcoes.js
+++ js/funcoes.js
@@ -14,22 +14,25 @@ function selectHeroi(){
     heroi = document.getElementById("heroi").value;
     let x = xmlDoc.getElementsByTagName("heroi");
     n = x.length-1;
-    console.log(n);
-    do{
+    do{ 
         if(heroi.toLowerCase()==x[n].getElementsByTagName("nome")[0].childNodes[0].nodeValue.toLowerCase()){
-            console.log(x[n].getElementsByTagName("nome")[0].childNodes[0].nodeValue.toLowerCase());
-            img_Heroi = x[n].getElementsByTagName("imagem")[0].childNodes[0].nodeValue;
+            if(x[n].getAttribute('universo')==universo.toLowerCase()){
+                img_Heroi = x[n].getElementsByTagName("imagem")[0].childNodes[0].nodeValue;
+            }else{
+                img_Heroi = "Universo inválido.";
+            }
             n=-1;
         }else{
             img_Heroi = "Herói inválido.";
             n--;
         }
+        
     }while(n>=0);
     mostrarHeroi();
 }
 
 function mostrarHeroi(){
-    if(img_Heroi=="Herói inválido."){
+    if(img_Heroi=="Herói inválido." || img_Heroi == "Universo inválido."){
         msgInvalido();
     }else{
         document.getElementById("box_heroi").style.display = "none";
@@ -42,11 +45,11 @@ function mostrarHeroi(){
 
 function msgInvalido(){
     document.getElementById("box_heroi").style.display = "none";
-        document.getElementById("img_heroi").style.display = "flex";
-        document.getElementById("fig_heroi").style.display = "none";
-        document.getElementById("msg_invalido").style.display = "block";
-        document.getElementById("msg_invalido").innerHTML = "<h1>:(</h1><h3>" + img_Heroi + "</h3>";
-        document.getElementById("img_heroi").innerHTML += "<button onclick='funcVoltar()' class='btn'>Voltar</button>";
+    document.getElementById("img_heroi").style.display = "flex";
+    document.getElementById("fig_heroi").style.display = "none";
+    document.getElementById("msg_invalido").style.display = "block";
+    document.getElementById("msg_invalido").innerHTML = "<h1>:(</h1><h3>" + img_Heroi + "</h3>";
+    document.getElementById("img_heroi").innerHTML += "<button onclick='funcVoltar()' class='btn'>Voltar</button>";
 }
 
 function funcVoltar(){
